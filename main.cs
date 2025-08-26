1f53fde43afeaf804be20832af1fee8da8e7819e
PyetroMarques <84328607+TrainerKiri@users.noreply.github.com>
2025-08-26 12:19:58 -0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,1 +1,17 @@
+using System;
+using System.Windows.Forms;
+using System.Data.SqlClient;
 
+namespace cadastroClienteApp
+{
+    public partial class MainForms : forms
+    public MainForm()
+    {
+        initializeComponent();
+    }
+}
+
+private void btnSalvar_Click(objetcs sender, EventArgs e) 
+{
+    SalvarDados Cliente(txtNome.Text, txtEndereco.Text, txtTelefone.Text,txtEmail.Text);
+}
